6e7aeb28fc681db989b10eea1f300a9ab2dcaa62
NateByers <nbyers@idem.in.gov>
2013-12-23 14:26:17 -0500

    Added more comments.

--- server.R
+++ server.R
@@ -3,19 +3,22 @@ library(openair)
 pete.ratts<-read.csv("PR_LocalMet_10m.csv")
 evansville<-read.csv("EVV10mMET_11103.csv")
 
-# Create a data.frame
+# Create a data frame
 wind.df<-data.frame(month=pete.ratts$Month, day=pete.ratts$JulianDay,
                    pr.ws=pete.ratts$WindSpeed, pr.wd=pete.ratts$WindDirection,
                    ev.ws=evansville$WindSpeed, ev.wd=evansville$WindDirection)
 
 # A windrose function that takes a date range and a site name
 wR <- function(range, site){
-    
+  
+  # subset wind speed and wind direction by day
   ws <- wind.df[wind.df$day %in% seq(range[1], range[2]), paste0(site, ".ws")]
   wd <- wind.df[wind.df$day %in% seq(range[1], range[2]), paste0(site, ".wd")]
   
+  # create data frame
   w.df <- data.frame(ws,wd)
   
+  # plot wind rose from openair package
   windRose(w.df,ws="ws",wd="wd",grid.line=5,annotate=FALSE,
              key.position="right",offset=3, breaks=5,paddle=FALSE,
              key.footer="Wind Speed (m/s)")  
@@ -27,7 +30,7 @@ wR <- function(range, site){
 # Define server logic 
 shinyServer(function(input, output) {
   
-  # Generate a plot 
+  # Generate the plot 
   output$Plot <- renderPlot({
     wR(range = input$range, site = input$site)
     })
--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ shinyUI(pageWithSidebar(
   # Application title
   headerPanel("Wind Rose, 2010"),
   
-  # Sidebar with controls to select the three parameters
+  # Sidebar with controls to select the date range and monitor
   sidebarPanel(
     sliderInput("range", "Range (Julian Day):",
                 min = 1, max = 365, value = c(1, 365)),
